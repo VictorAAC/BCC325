4703ca062504f52b564c3c2ce93c08f255857b25
VictorAAC <victor.coelho@aluno.ufop.edu.br>
2021-01-23 19:58:09 -0300

    comentarios

---
+++ definitions.py
@@ -0,0 +1,28 @@
+class Agent():
+    """
+    Essa classe implementa a interface para o agente
+    """
+    def __init__(self, env):
+        self.env = env
+
+    def act(self):
+        """
+        Este método permite que o agente execue uma ação no ambiente
+        """
+        raise NotImplementedError('act')
+
+class Environment:
+    """
+    Essa classe implementa a interface para o ambiente
+    """
+    def initial_percepts(self):
+        """
+        Este metodo retorna o estado do ambiente assim que o agente entra em contato com ele
+        """
+        raise NotImplementedError('initial_percepts')
+
+    def signal(self,action):
+        """
+        Este metodo retorna o estado do ambiente depois de sofrer uma açao do agente
+        """
+        raise NotImplementedError('signal')
